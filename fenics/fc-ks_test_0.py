655a294508662e778bc518ef3e59d7346ae7ef08
Alba María Navarro Izquierdo <alba_naizq96@hotmail.com>
2019-12-24 15:39:08 +0100

    Add files via upload

---
+++ fenics/fc-ks_test_0.py
@@ -0,0 +1,43 @@
+#
+# Flux-Correction Keller-Segel Test 0
+# ===================================
+#
+# In this test we use FEniCS for explicitly building and solving the
+# K-S FE equation system,that is, we explicitly buy FE matrices and
+# FE RHS vectors.
+#
+
+from keller_segel import KS_Matrix_DefaultScheme
+from numpy.testing import assert_approx_equal
+
+if( __name__ == "__main__" ):
+    #
+    # Read all the data from a parameters file
+    #
+    import data.chertok_kurganov as dat
+    dat.reset(_nx = 60)
+    #
+    # Define test as a concrete Keller-Segel scheme
+    #
+    ks_test = KS_Matrix_DefaultScheme( dat.mesh, dat.fe_order, dat.dt )
+    #
+    # Define C-K initial conditions
+    #
+    ks_test.set_u( dat.u_init )
+    ks_test.set_v( dat.v_init )
+    #
+    # Run time iterations
+    #
+    result = ks_test.run( nt_steps=100,
+                          break_when_negative_u=True, plot_u=False)
+
+    #
+    # Make sure that results match what happended in our previous tests!
+    #
+
+    # 1) Positivity is broken at iteration 10
+    assert result['iter']==10
+
+    # 2) max(u) is over 3.3*10^3
+    max_u = max(result['u'].vector())
+    assert_approx_equal(max_u, 3.328079e+03)
